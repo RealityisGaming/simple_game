07f4430a99f3993009058bb749da7c35fca1bab0
Matthew Hoecker <98118482+RealityisGaming@users.noreply.github.com>
2022-09-25 11:29:30 -0500

    Added game files to display

--- settings.py
+++ settings.py
@@ -6,12 +6,15 @@ screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Fantasy Survival')
 clock = pygame.time.Clock()
 
+test_surface = pygame.image.load('NinjaAdventure')
+
 while True:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       pygame.quit()
       exit()
   # draw elements
+  screen.blit(test_surface,(200,100))
   # update everything
   pygame.display.update()
   clock.tick(60)
