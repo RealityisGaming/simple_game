c5e34d928fecc0d275e7fefbc76914708cad2486
Matthew Hoecker <98118482+RealityisGaming@users.noreply.github.com>
2022-09-29 12:54:52 -0500

    Main Menu

--- settings.py
+++ settings.py
@@ -1,5 +1,6 @@
 import pygame
 from sys import exit
+from random import randint
 
 
 def display_score():
@@ -7,32 +8,58 @@ def display_score():
   score_surf = text_font.render(F'Score: {current_score}', False, (60,60,60))
   score_rect = score_surf.get_rect(center = (400,50))
   screen.blit(score_surf, score_rect)
+  return current_score
+
+def obstacle_movement(obstacle_list):
+  if obstacle_list:
+    for obstacle_rect in obstacle_list:
+      obstacle_rect.x -= 5
+
+      screen.blit(snail_surface,obstacle_rect)
+
+    return obstacle_list
+  else:
+    return []
 
 pygame.init()
 screen = pygame.display.set_mode((800,400))
 pygame.display.set_caption('Fantasy Survival')
 clock = pygame.time.Clock()
 text_font = pygame.font.Font(None, 50)
-game_active = True
+game_active = False
 start_time = 0
+score = 0
 
 sky_surface = pygame.image.load('graphics\Sky.png').convert()
 ground_surface = pygame.image.load('graphics\ground.png').convert()
 
-# score_surf = text_font.render('0', False, 'Red')
-# score_rect = score_surf.get_rect(topleft = (0, 0))
+score_surf = text_font.render('0', False, 'Red')
+score_rect = score_surf.get_rect(topleft = (0, 0))
 
-# Snail
-# snail_surface = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
-# snail_rect = snail_surface.get_rect(midbottom = (600, 300))
+# Obstacles
+snail_surface = pygame.image.load('graphics\snail\snail1.png').convert_alpha()
+snail_rect = snail_surface.get_rect(midbottom = (randint(900,1100), 300))
+obstacle_rect_list = []
 
 # Player
 player_surf = pygame.image.load('graphics\Player\player_walk_1.png').convert_alpha()
 player_rect= player_surf.get_rect(bottomleft = (80,300))
 player_gravity = 0
-player_stand = pygame.image.load('graphics\Player\player_stand.png').convert_alpha()
-player_stand_rect = player_stand.get_rect(bottomleft = (80,300))
 
+# Intro Screen
+player_stand = pygame.image.load('graphics\Player\player_stand.png').convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+player_stand_rect = player_stand.get_rect(center = (400,200))
+
+game_name = text_font.render('Fantasy Survival', False, (111, 196, 169))
+game_name_rect = game_name.get_rect(center = (400, 300))
+
+game_message = text_font.render('Press Space to run', False, (111, 196, 169))
+game_message_rect = game_message.get_rect(center = (400, 340))
+
+# Timer
+obstacle_timer = pygame.USEREVENT + 1
+pygame.time.set_timer(obstacle_timer, 1400)
 
 while True:
 
@@ -56,20 +83,27 @@ while True:
         snail_rect.left = 800
         start_time = int(pygame.time.get_ticks() / 1000)
 
-  # Elements
+    if event.type == obstacle_timer and game_active:
+      obstacle_rect_list.append(snail_surface.get_rect(midbottom = (randint(900,1100), 300)))
+      print('test')
+  
   if game_active:
+  # Elements
     screen.blit(sky_surface,(0,0))
     screen.blit(ground_surface,(0,300))
-    # pygame.draw.rect(screen, 'Pink', score_rect)
-    # screen.blit(score_surf,score_rect)
-    display_score()
+    pygame.draw.rect(screen, 'Pink', score_rect)
+    screen.blit(score_surf,score_rect)
+    score = display_score()
 
-  # Snail
+    # Snail
     # snail_rect.right -= 4
     # if snail_rect.right <=0: snail_rect.left = 800
     # screen.blit(snail_surface, snail_rect)
 
-  # Player
+    # Obstacle Motion
+    obstacle_rect_list = obstacle_movement(obstacle_rect_list)
+
+    # Player
     player_gravity += 1
     player_rect.y += player_gravity
     if player_rect.bottom >= 300:
@@ -77,12 +111,23 @@ while True:
     
     screen.blit(player_surf, player_rect)
 
-    # # Collsion
-    # if snail_rect.colliderect(player_rect):
-    #   game_active = False
+    # Collsion
+    if snail_rect.colliderect(player_rect):
+      game_active = False
   else:
     screen.fill((94,129,162))
     screen.blit(player_stand, player_stand_rect)
+
+    score_message = text_font.render(f'Your Score: {score}', False, (111, 196, 169))
+    score_message_rect = score_message.get_rect(center = (400, 330))
+    screen.blit(game_name, game_name_rect)
+    
+    if score == 0:
+      screen.blit(game_message, game_message_rect)
+    else:
+      screen.blit(score_message, score_message_rect)
+
+
 
 
   # update everything
